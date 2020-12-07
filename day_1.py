9d6613a46defa0a44130ffa139aecbd55bad0914
Iain Cattermole <iain.cattermole@skyscanner.net>
2020-12-07 13:52:22 +0000

    Day one

---
+++ day_1.py
@@ -0,0 +1,36 @@
+from itertools import combinations
+import sys
+
+if len(sys.argv) < 2:
+    print("Must specify input file")
+    exit(1)
+
+
+with open(sys.argv[1]) as f:
+    expense_report = [int(entry) for entry in f.readlines()]
+
+
+def find_first_sum_combination(expense_report, sum_total, group_size):
+    for comb in combinations(expense_report, group_size):
+        if sum(comb) == sum_total:
+            return comb
+
+
+def multiply(*args):
+    if len(args) < 1:
+        return None
+
+    result = 1
+    for num in args:
+        result *= num
+    return result
+
+
+sum_total = 2020 
+group_size = int(sys.argv[2]) if len(sys.argv) > 2 else 2  # Default part 1
+
+result = find_first_sum_combination(expense_report, sum_total, group_size)
+if result:
+    print(f"Answer: {multiply(*result)}")
+else:
+    print("No pair matching the target sum")
